61b69b0a235b07e31c7d6586e8ac05631d76451a
aembadawy <aem.badawy@gmail.com>
2025-04-06 21:36:32 +0200

    Initial Commit

---
+++ quiz-swiftui/quiz_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  quiz_swiftuiApp.swift
+//  quiz-swiftui
+//
+//  Created by Aya on 06/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct quiz_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
