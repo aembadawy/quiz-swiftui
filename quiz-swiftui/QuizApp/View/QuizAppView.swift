2015626d46f80f1c0e5b10dad6cd758f0d876605
aembadawy <aem.badawy@gmail.com>
2025-04-09 10:08:58 +0200

    Add quiz view

--- quiz-swiftui/QuizApp/View/QuizAppView.swift
+++ quiz-swiftui/QuizApp/View/QuizAppView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct QuizAppView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 8) {
+            Text("What is the function of view model in MVVM architecture?")
+                .font(.headline)
+                .fontWeight(.semibold)
+            
+            
+            Text("Select one answer")
+                .foregroundStyle(.gray)
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: 120)
+        .padding()
+        
+        VStack(alignment: .leading){
+            Text("Question 1 / 6")
+                .font(.subheadline)
+                .padding(.horizontal)
+            Divider()
+        }
+        .padding(.bottom)
+        
+        VStack(spacing: 12) {
+            ForEach(0 ..< 4, id: \.self) { choice in
+                
+                Button {
+                    //select answer
+                } label: {
+                    Text("Display answer here for now")
+                        .padding(.horizontal)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(.black)
+                        .frame(width: 350, height: 48, alignment: .leading)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(.gray, lineWidth: 1.0)
+
+                        }
+                }.padding(.horizontal, 8)
+            }
+        }
+        
+        Spacer()
+        
+        Button {
+            //submit answer
+        } label: {
+            Text("Submit")
+                .padding(.horizontal)
+                .font(.subheadline)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+                .frame(width: 350, height: 48, alignment: .center)
+                .background(.blue)
+                .clipShape(.rect(cornerRadius: 10))
+                
+        }.padding(.all, 8)
+        
     }
 }
 
