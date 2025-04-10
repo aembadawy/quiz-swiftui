f7024e6e9375b7e15d80b62d3004fd4af0d77db4
aembadawy <aem.badawy@gmail.com>
2025-04-10 15:44:37 +0200

    Add the completed quiz view

---
+++ quiz-swiftui/QuizApp/View/CompletedQuizView.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedQuizView.swift
+//  quiz-swiftui
+//
+//  Created by Aya on 10/04/2025.
+//
+
+import SwiftUI
+
+struct CompletedQuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedQuizView()
+}
