1538050e73aa560ca7131599178b219a263445df
aembadawy <aem.badawy@gmail.com>
2025-04-10 20:59:46 +0200

    Add root view to content view

---
+++ quiz-swiftui/QuizApp/View/QuizResultsView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizResultsView.swift
+//  quiz-swiftui
+//
+//  Created by Aya on 10/04/2025.
+//
+
+import SwiftUI
+
+struct QuizResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizResultsView()
+}
